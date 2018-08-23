c6b402ffd41651c0f3159709dff1fa7bfdc87f84
Gowtham171996 <bcgowtham17@gmail.com>
2018-08-23 18:50:38 +0530

    Hello World Linear regression

---
+++ lr.py
@@ -0,0 +1,32 @@
+
+import warnings
+with warnings.catch_warnings():
+    warnings.simplefilter("ignore", DeprecationWarning)
+    import pandas as pd
+    import numpy as np
+    import matplotlib.pyplot as plt
+    from sklearn.cross_validation import train_test_split
+
+dataset = pd.read_csv('studentscores.csv')
+X = dataset.iloc[ : ,   : 1 ].values
+Y = dataset.iloc[ : , 1 ].values
+
+
+X_train, X_test, Y_train, Y_test = train_test_split( X, Y, test_size = 1/4, random_state = 0)
+
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor = regressor.fit(X_train, Y_train)
+
+Y_pred = regressor.predict(X_test)
+
+plt.scatter(X_train , Y_train, color = 'red')
+plt.plot(X_train , regressor.predict(X_train), color ='blue')
+plt.show()
+
+
+plt.scatter(X_test , Y_test, color = 'red')
+plt.plot(X_test , regressor.predict(X_test), color ='blue')
+plt.show()
+
+print("Finished")
